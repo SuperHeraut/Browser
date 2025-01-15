e5f346a1bd9ae839af90450cef0583af118c4960
SuperHeraut <super.heraut.officiel@gmail.com>
2025-01-15 04:15:12 +0100

    feat: create variables.js + navigation.js

---
+++ navigation.js
@@ -0,0 +1,21 @@
+BACKBTN.addEventListener("click", goBack);
+FORWARDBTN.addEventListener("click", goForward);
+REFRESHBTN.addEventListener("click", refreshPage);
+
+function goBack(){
+
+};
+
+function goForward(){
+	
+};
+
+function refreshPage(){
+	
+};
+
+URLINPUT.form.addEventListener("submit", loadPage);
+
+function loadPage(e){
+	e.preventDefault();
+};
---
+++ variables.js
@@ -0,0 +1,6 @@
+const BACKBTN = document.getElementById("back");
+const FORWARDBTN = document.getElementById("forward");
+const REFRESHBTN = document.getElementById("refresh");
+const URLINPUT = document.getElementById("url");
+
+let url = URLINPUT.value;
